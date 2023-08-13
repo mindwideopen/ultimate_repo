f51e56ad50a578a838367fa64134c85368dd84fb
mindwideopen <nlit3926@gmail.com>
2023-08-13 11:00:08 +0200

    13 august

--- codewars/8kyu/Removing Elements.js
+++ codewars/8kyu/Removing Elements.js
@@ -5,3 +5,18 @@
 // ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
 // None of the arrays will be empty, so you don't have to worry about that!
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10,11];
+
+let newArray = [];
+
+for (let i=0; i< arr.length; i+=2) {
+    newArray.push(arr[i]);
+     
+};
+
+
+
+
+console.log(newArray);
